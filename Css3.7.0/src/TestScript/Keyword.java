9c79e32bda312cd8f49424e681057ab936fb179d
name <name@192.168.0.110>
2020-01-15 17:39:19 +0530

    commits

--- Css3.7.0/src/TestScript/Keyword.java
+++ Css3.7.0/src/TestScript/Keyword.java
@@ -1,5 +1,4 @@
 package TestScript;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -10,9 +9,6 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-
-
 public class Keyword {
 	public static void main(String[] args) throws IOException {
 		ArrayList a = new ArrayList();
@@ -37,11 +33,8 @@ public class Keyword {
 				case BOOLEAN:
 					a.add(celldata.getBooleanCellValue());
 					break;
-					
 				}
-				
 			}
-			
 		}
 		
 		for(int i=0;i<a.size();i++) {
@@ -53,9 +46,7 @@ public class Keyword {
 				System.out.println(Keyword);
 				System.out.println(deta);
 				System.out.println(objectname);
-				System.out.println( runmode);
-				
-				
+				System.out.println( runmode);	
 			}
 		}
 		
@@ -68,14 +59,47 @@ public class Keyword {
 				System.out.println(Keyword);
 				System.out.println(deta);
 				System.out.println(objectname);
-				System.out.println( runmode);
-				
-				
+				System.out.println( runmode);	
 			}
 		}
 		
+		for(int i=0;i<a.size();i++) {
+			if(a.get(i).equals("input")) {
+				String Keyword = (String)a.get(i);
+				String deta =(String)a.get(i+1);
+				String objectname =(String)a.get(i+2);
+				String runmode =(String)a.get(i+3);
+				System.out.println(Keyword);
+				System.out.println(deta);
+				System.out.println(objectname);
+				System.out.println( runmode);
+			}
+		}
 		
-		
+		for(int i=0;i<a.size();i++) {
+			if(a.get(i).equals("input")) {
+				String Keyword = (String)a.get(i);
+				String deta =(String)a.get(i+1);
+				String objectname =(String)a.get(i+2);
+				String runmode =(String)a.get(i+3);
+				System.out.println(Keyword);
+				System.out.println(deta);
+				System.out.println(objectname);
+				System.out.println( runmode);	
+			}
+		}
+		for(int i=0;i<a.size();i++) {
+			if(a.get(i).equals("click")) {
+				String Keyword = (String)a.get(i);
+				String deta =(String)a.get(i+1);
+				String objectname =(String)a.get(i+2);
+				String runmode =(String)a.get(i+3);
+				System.out.println(Keyword);
+				System.out.println(deta);
+				System.out.println(objectname);
+				System.out.println( runmode);
+			}
+		}
 		
 		
 		
