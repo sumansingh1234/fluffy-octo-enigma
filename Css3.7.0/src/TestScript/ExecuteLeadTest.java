939978eeb6be551356c11e3cb7af31fcaa6703a8
name <name@192.168.0.110>
2020-01-16 20:23:40 +0530

    commits

--- Css3.7.0/src/TestScript/ExecuteLeadTest.java
+++ Css3.7.0/src/TestScript/ExecuteLeadTest.java
@@ -1,5 +1,19 @@
 package TestScript;
 
-public class ExecuteLeadTest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+public class ExecuteLeadTest {
+	public static void main(String[] args) {
+	FirefoxDriver Driver = new FirefoxDriver();
+	WebDriver driver=new ChromeDriver();	
+	driver.get("https:facebook.com");
 }
+	
+	
+	
+	
+	}
+	
+
--- Css3.7.0/src/TestScript/Keyword.java
+++ Css3.7.0/src/TestScript/Keyword.java
@@ -4,12 +4,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Keyword {
-
+    static String deta="https:www.facebook.com";
 	@Test
-	public void openbrowser() {
+	public void openbrowser(String deta) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\name\\Documents\\chrome driver\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();	
-		driver.get("https:facebook.com");
+		driver.get(deta);
 	}
 
 }
